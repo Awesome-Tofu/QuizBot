8f244d4fd0729efce0ecc3c5df027b337ea4c9ce
awesome-tofu <adityaraj6311@gmail.com>
2024-01-13 17:42:14 +0530

    hmm

--- QuizBot/structure.js
+++ QuizBot/structure.js
@@ -3,11 +3,11 @@ const register = require('./modules/register.js');
 
 module.exports = async function structure(client, msg) {
     const quotedMsg = await msg.getQuotedMessage();
-    const startMsg = "Hello, I am quiz Bot. I can help you play quizes and check your knowledge. Please *reply this message* with follwing **number** you want to play\n\n1️⃣Social Affairs\n2️⃣Quiz" 
+    const startMsg = "Hello, I am quiz Bot. I can help you play quizes and check your knowledge. Please *reply this message* with follwing *number* you want to play\n\n1️⃣Social Affairs\n2️⃣Quiz"
 
     const number = msg.from.replace('@c.us', '');
     const isInDatabase = await isUserInDatabase(msg);
-    const isPaid = await isUserPaid(msg);    
+    const isPaid = await isUserPaid(msg);
 
     if (quotedMsg && quotedMsg.body === startMsg && msg.body === "1") {
         await msg.reply("Social Affairs Quiz Started")
@@ -15,10 +15,10 @@ module.exports = async function structure(client, msg) {
         await msg.reply("Quiz Started")
     } else if (!isInDatabase) {
         await register(msg, number);
-    } else if(isInDatabase && !isPaid){
-        await msg.reply("Thanks for registering.\nYou can pay the amount of Rs.X here:\n"+ `https://pay-site.vercel.app/${number}`);
+    } else if (isInDatabase && !isPaid) {
+        await msg.reply("Thanks for registering.\nYou can pay the amount of Rs.X here:\n" + `https://pay-site.vercel.app/${number}`);
         return;
-    }else {
+    } else {
         await msg.reply(startMsg);
     }
 }
